46cd9ba8981a7e074ff03b439fc672b0f01c06ff
978924688@qq.com <978924688@qq.com>
2020-03-21 17:44:24 +0800

    c++ vscode

---
+++ Calculator.cpp
@@ -0,0 +1,26 @@
+#include "Calculator.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main(void)
+{
+    AbstractCalculator *p = new Add(3,7);
+    cout << p->m_Val_1 << "+" << p->m_Vla_2 << "=" << p->get_Result() << endl;
+
+    p = new Minus(4,8);
+    cout << p->m_Val_1 << "-" << p->m_Vla_2 << "=" << p->get_Result() << endl;
+
+    p = new Mul(10,2);
+    cout << p->m_Val_1 << "x" << p->m_Vla_2 << "=" << p->get_Result() << endl;
+    delete p;
+
+    system("pause");
+    return 0;
+}
